1b7e427ed3a7bcb0493d694cdb4bf2c0d39e3b04
sooboi <dlgbrkd123@gmail.com>
2022-07-26 20:14:27 +0900

    Add : 리스트 처리,삭제 기능추가 구현

--- main.js
+++ main.js
@@ -5,22 +5,62 @@ let taskList = [];
 addBtn.addEventListener("click", addTask);
 
 function addTask() {
-  let taskContent = taskInput.value;
-  taskList.push(taskContent);
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
   render();
+  console.log(task);
 }
 
 function render() {
   let resultHTML = "";
   for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `<div class="task">
-    <div>${taskList[i]}</div>
-            <div>
-              <button>check</button>
-              <button>delete</button>
-            </div>
-          </div>
-    </div>`;
+    if (taskList[i].isComplete == true) {
+      resultHTML += `<div class="task">
+        <div class="task-done">${taskList[i].taskContent}</div>
+                <div>
+                  <button onClick="toggleComplete('${taskList[i].id}')">check</button>
+                  <button onClick="deleteTask('${taskList[i].id}')">delete</button>
+                </div>
+              </div>
+        </div>`;
+    } else {
+      resultHTML += `<div class="task">
+        <div>${taskList[i].taskContent}</div>
+        <div>
+        <button onClick="toggleComplete('${taskList[i].id}')">check</button>
+        <button onClick="deleteTask('${taskList[i].id}')">delete</button>
+        </div>
+        </div>
+        </div>`;
+    }
   }
   document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+}
+
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
